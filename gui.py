13a9f128c671850167d0308009663ffd644b9649
Abdelhak Chellal <125835174+Abdelhak-Chellal@users.noreply.github.com>
2023-05-25 22:02:27 +0100

    link gui

--- gui.py
+++ gui.py
@@ -1,13 +1,9 @@
-import main
-import tkinter as tk
-from problem import Problem
-import networkx as nx
-from matplotlib.animation import FuncAnimation
+from csp import *
 import matplotlib.pyplot as plt
-
-# importing the file of the game
-import sys
-sys.path.insert(1, 'connect 4/main.py')
+from matplotlib.animation import FuncAnimation
+import networkx as nx
+from problem import Problem
+import tkinter as tk
 
 
 def get_user_edge(entry_1, entry_2, entry_3, p):
@@ -50,8 +46,48 @@ def delete_content(root):
         widget.destroy()
 
 
-# path parameter here will be replaced when calling this function with
-    # the functions of uninformed or infomed search
+# read the variable label and set its name
+def get_variable_label(root, label, csp):
+    var.name = label
+    # call the menu of inserting the domain of this variable
+    csp_domain_interface(root, csp)
+
+
+# read a value for the variable domain
+def get_variable_value(root, value, csp):
+    # adding value for the domain of the variable
+    var.add_value_domain(value.get())
+    value.delete(0, tk.END)
+
+# read the constraints in order to add them to the CSP
+
+
+# get the format of constraints in order to inseret them
+def get_constraints(different, equal, root, csp):
+    diff = different.split(",")
+    eq = equal.split(",")
+    csp.add_constraint(AllDifferentConstraint(diff))
+    csp.add_constraint(AllEqualConstraint(eq))
+    CSP_solver(root, csp)
+
+# get the format of constraints in order to inseret them
+
+
+def csp_solutions(csp):  # function to print all the possible assignments of variables
+    sol = csp.get_solutions()
+    for s in sol:
+        assignment = ""
+        first = True
+        for key in s.keys():
+            assignment = assignment + key + " = " + s[key] + " ,"
+        if assignment != "":
+            label = tk.Label(root, text=assignment, font=('Arial', 20))
+            label.pack(padx=20, pady=40)
+        else:
+            label = tk.Label(
+                root, text="No assignment found for your CSP", font=('Arial', 20))
+            label.pack(padx=20, pady=40)
+
 
 def animate_solution(p, path):
     init_pos = nx.spring_layout(p.graph)
@@ -86,35 +122,40 @@ def animate_solution(p, path):
     plt.show()
 
 
-def main_menu(p, sender=0):
+def main_menu(p, csp=0, sender=0):
     if sender != 0:  # check if it is called from onther menu
         delete_content(sender)
     else:
         global root
         root = tk.Tk()  # parent window
-
+    p.__init__([])
     width, height = 650, 800
     v_dim = str(width)+'x'+str(height)
     root.geometry(v_dim)  # Size of the window
-    root.title("AI Project")  # Adding a title
+    root.title("Your AI Toolbox")  # Adding a title
 
     # Creating the Frame for the GUI
     frame = tk.Frame(root, bg='#80c1ff', bd=5)
     frame.place(relwidth=1, relheight=1)
 
-    label = tk.Label(root, text="Welcome!", font=('Arial', 30))
-    label.pack(padx=20, pady=30)
+    label = tk.Label(root, text=" Welcome! ", font=('Arial', 30))
+    label.pack(padx=20, pady=100)
 
-    button = tk.Button(root, text="Define your problem", font=(
+    button = tk.Button(root, text=" Define your problem ", font=(
         'Arial', 18), command=lambda: problem_interface(root, p))  # call new menu to enter the problem
     button.pack(padx=10, pady=10)
 
-    button = tk.Button(root, text="    Play Game   ", font=('Arial', 18))
+    button = tk.Button(root, text="    Define your CSP    ", font=(
+        'Arial', 18), command=lambda: csp_variable_interface(root, csp))  # call new menu to enter the problem
+    button.pack(padx=10, pady=10)
+
+    button = tk.Button(
+        root, text="         Play Game        ", font=('Arial', 18))
     button.pack(padx=10, pady=10)
 
     button = tk.Button(root, text="         Exit         ",
                        font=('Arial', 18), command=lambda: exit())
-    button.pack(padx=10, pady=10)
+    button.pack(padx=10, pady=100)
 
     root.mainloop()  # Keep the window open
 
@@ -126,7 +167,7 @@ def problem_interface(sender, p):
     width, height = 650, 800
     v_dim = str(width)+'x'+str(height)
     root.geometry(v_dim)  # Size of the window
-    root.title("AI Project")  # Adding a title
+    root.title("Your AI Toolbox")  # Adding a title
 
     # Creating the Frame for the GUI
     frame = tk.Frame(root, bg='#80c1ff', bd=5)
@@ -134,7 +175,7 @@ def problem_interface(sender, p):
 
     label = tk.Label(
         root, text="Define your problem by entering nodes", font=('Arial', 20))
-    label.pack(padx=20, pady=30)
+    label.pack(padx=20, pady=70)
 
     label = tk.Label(
         root, text="Node label", font=('Arial', 20))
@@ -155,18 +196,193 @@ def problem_interface(sender, p):
     # need to know the structure to implement the function
     button = tk.Button(root, text="Add node", font=(
         'Arial', 18), command=lambda: get_node_info(node_label, node_value, p))
-    button.pack(padx=10, pady=10)
+    button.pack(padx=10, pady=20)
 
     button = tk.Button(root, text="         Exit         ",
                        font=('Arial', 18), command=lambda: exit())
     button.pack(padx=10, pady=10, side="bottom")
 
     button = tk.Button(root, text="    Main menu   ", font=(
-        'Arial', 18), command=lambda: main_menu(root, p))
+        'Arial', 18), command=lambda: main_menu(p, csp, root))
     button.pack(padx=10, pady=10, side="bottom")
 
-    button = tk.Button(root, text="    Add the initial state   ", font=(
+    button = tk.Button(root, text="     Add the initial state    ", font=(
         'Arial', 18), command=lambda: initial_state_interface(root, p))
+    button.pack(padx=10, pady=10, side="bottom")
+
+    root.mainloop()  # Keep the window open
+
+
+def csp_variable_interface(sender, csp):
+
+    delete_content(sender)
+
+    global var
+    var = Variable()  # creat a variable in order to insert it to the csp
+
+    if var != None:
+        csp.add_variable(var)
+
+    width, height = 650, 800
+    v_dim = str(width)+'x'+str(height)
+    root.geometry(v_dim)  # Size of the window
+    root.title("Your AI Toolbox")  # Adding a title
+
+    # Creating the Frame for the GUI
+    frame = tk.Frame(root, bg='#80c1ff', bd=5)
+    frame.place(relwidth=1, relheight=1)
+
+    label = tk.Label(
+        root, text="Define your CSP by entering your variables and constraints", font=('Arial', 20))
+    label.pack(padx=20, pady=70)
+
+    label = tk.Label(
+        root, text="Variable label", font=('Arial', 20))
+    label.pack(padx=10, pady=10)
+
+    # Create an Entry for lebel of that node
+    variable_label = tk.Entry(root)
+    variable_label.pack()
+
+    # need to know the structure to implement the function
+    button = tk.Button(root, text="Add a variable", font=(
+        'Arial', 18), command=lambda: get_variable_label(root, variable_label.get(), csp))
+    button.pack(padx=10, pady=20)
+
+    button = tk.Button(root, text="         Exit         ",
+                       font=('Arial', 18), command=lambda: exit())
+    button.pack(padx=10, pady=10, side="bottom")
+
+    button = tk.Button(root, text="    Main menu   ", font=(
+        'Arial', 18), command=lambda: main_menu(p, csp, root))
+    button.pack(padx=10, pady=10, side="bottom")
+
+    root.mainloop()  # Keep the window open
+
+
+def csp_domain_interface(sender, csp):
+
+    delete_content(sender)
+
+    width, height = 650, 800
+    v_dim = str(width)+'x'+str(height)
+    root.geometry(v_dim)  # Size of the window
+    root.title("Your AI Toolbox")  # Adding a title
+
+    # Creating the Frame for the GUI
+    frame = tk.Frame(root, bg='#80c1ff', bd=5)
+    frame.place(relwidth=1, relheight=1)
+
+    label = tk.Label(
+        root, text="Enter the domain of the variable you entered", font=('Arial', 20))
+    label.pack(padx=20, pady=70)
+
+    label = tk.Label(
+        root, text="Domain's value", font=('Arial', 20))
+    label.pack(padx=10, pady=10)
+
+    # Create an Entry for lebel of that node
+    variable_value = tk.Entry(root)
+    variable_value.pack()
+
+    # need to know the structure to implement the function
+    button = tk.Button(root, text="Add value", font=(
+        'Arial', 18), command=lambda: get_variable_value(root, variable_value, csp))
+    button.pack(padx=10, pady=20)
+
+    button = tk.Button(root, text="         Exit         ",
+                       font=('Arial', 18), command=lambda: exit())
+    button.pack(padx=10, pady=10, side="bottom")
+
+    button = tk.Button(root, text="    Main menu   ", font=(
+        'Arial', 18), command=lambda: main_menu(p, csp, root))
+    button.pack(padx=10, pady=10, side="bottom")
+
+    button = tk.Button(root, text=" Define your constraints  ", font=(
+        'Arial', 18), command=lambda: constraints_interface(root, csp))
+    button.pack(padx=10, pady=10, side="bottom")
+
+    button = tk.Button(root, text="     Add more variables    ", font=(
+        'Arial', 18), command=lambda: csp_variable_interface(root, csp))
+    button.pack(padx=10, pady=10, side="bottom")
+
+    root.mainloop()  # Keep the window open
+
+
+def constraints_interface(sender, csp):
+
+    delete_content(sender)
+
+    width, height = 650, 800
+    v_dim = str(width)+'x'+str(height)
+    root.geometry(v_dim)  # Size of the window
+    root.title("Your AI Toolbox")  # Adding a title
+
+    # Creating the Frame for the GUI
+    frame = tk.Frame(root, bg='#80c1ff', bd=5)
+    frame.place(relwidth=1, relheight=1)
+
+    label = tk.Label(
+        root, text="PS: Follow the format: variable1, variable2, ...", font=('Arial', 20))
+    label.pack(padx=10, pady=70)
+
+    label = tk.Label(
+        root, text="Enter the variables that must be equal", font=('Arial', 20))
+    label.pack(padx=20, pady=20)
+
+    # Create an Entry for lebel of that node
+    equale_variables = tk.Entry(root)
+    equale_variables.pack()
+
+    label = tk.Label(
+        root, text="Enter the variables that must be different", font=('Arial', 20))
+    label.pack(padx=20, pady=20)
+
+    # Create an Entry for lebel of that node
+    different_variables = tk.Entry(root)
+    different_variables.pack()
+
+    # need to know the structure to implement the function
+    button = tk.Button(root, text="Solve the CSP subject to this constrains", font=(
+        'Arial', 18), command=lambda: get_constraints(different_variables.get(), equale_variables.get(), root, csp))
+    button.pack(padx=10, pady=30)
+
+    button = tk.Button(root, text="         Exit         ",
+                       font=('Arial', 18), command=lambda: exit())
+    button.pack(padx=10, pady=10, side="bottom")
+
+    button = tk.Button(root, text="    Main menu   ", font=(
+        'Arial', 18), command=lambda: main_menu(p, csp, root))
+    button.pack(padx=10, pady=10, side="bottom")
+
+    root.mainloop()  # Keep the window open
+
+
+def CSP_solver(sender, csp):
+
+    delete_content(sender)
+
+    width, height = 650, 800
+    v_dim = str(width)+'x'+str(height)
+    root.geometry(v_dim)  # Size of the window
+    root.title("Your AI Toolbox")  # Adding a title
+
+    # Creating the Frame for the GUI
+    frame = tk.Frame(root, bg='#80c1ff', bd=5)
+    frame.place(relwidth=1, relheight=1)
+
+    label = tk.Label(
+        root, text="The possible assignment(s) for the variables is(are):", font=('Arial', 20))
+    label.pack(padx=10, pady=40)
+
+    csp_solutions(csp)
+
+    button = tk.Button(root, text="         Exit         ",
+                       font=('Arial', 18), command=lambda: exit())
+    button.pack(padx=10, pady=10, side="bottom")
+
+    button = tk.Button(root, text="    Main menu   ", font=(
+        'Arial', 18), command=lambda: main_menu(p, csp, root))
     button.pack(padx=10, pady=10, side="bottom")
 
     root.mainloop()  # Keep the window open
@@ -178,7 +394,7 @@ def initial_state_interface(sender, p):
     width, height = 650, 800
     v_dim = str(width)+'x'+str(height)
     root.geometry(v_dim)  # Size of the window
-    root.title("AI Project")  # Adding a title
+    root.title("Your AI Toolbox")  # Adding a title
 
     # Creating the Frame for the GUI
     frame = tk.Frame(root, bg='#80c1ff', bd=5)
@@ -186,7 +402,7 @@ def initial_state_interface(sender, p):
 
     label = tk.Label(
         root, text="Define your initial state", font=('Arial', 20))
-    label.pack(padx=20, pady=30)
+    label.pack(padx=20, pady=70)
 
     label = tk.Label(
         root, text="Initial state label", font=('Arial', 20))
@@ -199,7 +415,7 @@ def initial_state_interface(sender, p):
     # need to know the structure to implement the function
     button = tk.Button(root, text="Add ", font=('Arial', 18),
                        command=lambda: add_initial_state(root, entry, p))
-    button.pack(padx=10, pady=10)
+    button.pack(padx=10, pady=20)
 
     button = tk.Button(root, text="         Exit         ",
                        font=('Arial', 18), command=lambda: exit())
@@ -218,7 +434,7 @@ def goal_states_interface(sender, p):
     width, height = 650, 800
     v_dim = str(width)+'x'+str(height)
     root.geometry(v_dim)  # Size of the window
-    root.title("AI Project")  # Adding a title
+    root.title("Your AI Toolbox")  # Adding a title
 
     # Creating the Frame for the GUI
     frame = tk.Frame(root, bg='#80c1ff', bd=5)
@@ -226,7 +442,7 @@ def goal_states_interface(sender, p):
 
     label = tk.Label(
         root, text="Define your goal states", font=('Arial', 20))
-    label.pack(padx=20, pady=30)
+    label.pack(padx=20, pady=70)
 
     label = tk.Label(
         root, text="Node label", font=('Arial', 20))
@@ -239,7 +455,7 @@ def goal_states_interface(sender, p):
     # need to know the structure to implement the function
     button = tk.Button(root, text="Add a goal", font=(
         'Arial', 18),  command=lambda: p.add_goal_state(entry.get()))  # call the fuction to add a goal state to the problem
-    button.pack(padx=10, pady=10)
+    button.pack(padx=10, pady=20)
 
     button = tk.Button(root, text="         Exit         ",
                        font=('Arial', 18), command=lambda: exit())
@@ -266,7 +482,7 @@ def edges_interface(sender, p):
     width, height = 650, 800
     v_dim = str(width)+'x'+str(height)
     root.geometry(v_dim)  # Size of the window
-    root.title("AI Project")  # Adding a title
+    root.title("Your AI Toolbox")  # Adding a title
 
     # Creating the Frame for the GUI
     frame = tk.Frame(root, bg='#80c1ff', bd=5)
@@ -274,7 +490,7 @@ def edges_interface(sender, p):
 
     label = tk.Label(
         root, text="Define your edges", font=('Arial', 20))
-    label.pack(padx=20, pady=30)
+    label.pack(padx=20, pady=70)
 
     label = tk.Label(
         root, text="Node 1", font=('Arial', 20))
@@ -302,7 +518,7 @@ def edges_interface(sender, p):
 
     button = tk.Button(root, text="Add edge", font=(
         'Arial', 18), command=lambda: get_user_edge(entry_1, entry_2, entry_3, p))
-    button.pack(padx=10, pady=10)
+    button.pack(padx=10, pady=20)
 
     button = tk.Button(root, text="         Exit         ",
                        font=('Arial', 18), command=lambda: exit())
@@ -335,7 +551,7 @@ def algorithms_menu(p, sender=0):
     width, height = 650, 800
     v_dim = str(width)+'x'+str(height)
     root.geometry(v_dim)  # Size of the window
-    root.title("AI Project")  # Adding a title
+    root.title("Your AI Toolbox")  # Adding a title
 
     # Creating the Frame for the GUI
     frame = tk.Frame(root, bg='#80c1ff', bd=5)
@@ -405,11 +621,12 @@ def algorithms_menu(p, sender=0):
     button.pack(padx=10, pady=10, side="bottom")
 
     button = tk.Button(root, text="    Main menu   ", font=(
-        'Arial', 18), command=lambda: main_menu(root, p))
+        'Arial', 18), command=lambda: main_menu(root, csp,  p))
     button.pack(padx=10, pady=15, side="bottom")
 
     root.mainloop()  # Keep the window open
 
 
 p = Problem([])
-main_menu(p)
+csp = CSP()
+main_menu(p, csp)
